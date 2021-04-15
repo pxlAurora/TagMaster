c498e8e2d34315faadd65dc5714eac0a72134e30
pxlAurora <user@localhost>
2021-04-15 17:48:11 +0200

    Remove pako

--- src/worker/tagData.ts
+++ src/worker/tagData.ts
@@ -1,10 +1,6 @@
 import {TagData} from '../common/types';
-// @ts-ignore
-// import * as pako from 'pako';
 
 declare function __webpack_require__(path: string): any;
 
 const data = __webpack_require__(require.resolve('tag-data')) as TagData;
 export default data;
-// @ts-ignore
-// window.pako = pako;
